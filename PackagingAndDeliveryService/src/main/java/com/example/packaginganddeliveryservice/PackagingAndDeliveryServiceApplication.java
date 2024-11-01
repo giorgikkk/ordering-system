a7fe0676141326d47c5cfdca3261e738aa94ad43
giorgikkk <kgiorrgi@gmail.com>
2024-11-02 01:31:28 +0400

    Initialized services with Spring Boot projects

---
+++ PackagingAndDeliveryService/src/main/java/com/example/packaginganddeliveryservice/PackagingAndDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.packaginganddeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagingAndDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagingAndDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ PackagingAndDeliveryService/src/test/java/com/example/packaginganddeliveryservice/PackagingAndDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.packaginganddeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagingAndDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
